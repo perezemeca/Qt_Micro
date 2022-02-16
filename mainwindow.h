f8fc705aa4a1389c0fc9a6e51d3f777326e254c7
Emmanuel <pereze.meca@gmail.com>
2022-02-16 16:20:23 -0300

    UI -> OK // Enviar Comandos -> NO OK

--- mainwindow.h
+++ mainwindow.h
@@ -52,21 +52,11 @@ private slots:
 
     void on_pushButton_3_clicked();
 
-    //void on_horizontalSlider_valueChanged(int value);
-
-    //void on_horizontalSlider_sliderMoved(int position);
-
     void on_verticalSlider_2_valueChanged(int value);
 
     void on_verticalSlider_valueChanged(int value);
 
-    //void on_horizontalSlider_actionTriggered(int action);
-
-    //void on_verticalSlider_4_valueChanged(int value);
-
     void on_pushButton_5_clicked();
-
-    void on_pushButton_4_clicked();
 
 private:
     Ui::MainWindow *ui;
@@ -75,13 +65,11 @@ private:
     QSerialPort *QSerialPort1;
 
     uint8_t rx[256], header, timeout, nbytes, cks, index, time1, valuechanged_1, valuechanged_2;
+    uint16_t Counter = 0;
 
     QUdpSocket *QUdpSocket1;
     QHostAddress hostAddress;     //Direccion de quien envio
     quint16 hostPort;             // Puerto de quien envio
-
-    uint8_t buff_UDP[256];
-    uint16_t indW_UDP=0,indR_UDP=0;
 
     void DecodeCmd(uint8_t *RX);
     void SendCmd(uint8_t *buf, uint8_t length);
